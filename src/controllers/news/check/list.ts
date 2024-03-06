ac0b889a39f0d4734bebfa5d6fcda040ed6520df
killgram <killgram94@gmail.com>
2024-03-07 02:07:59 +0500

    lock AL service

--- src/controllers/news/check/list.ts
+++ src/controllers/news/check/list.ts
@@ -1,15 +1,10 @@
 import { NewsSource } from "@enums";
 
 import { getSHNews } from "./sh";
-import { getALNews } from "./al";
 
 export const checkList = [
   {
     name: NewsSource.SH,
     logicFunction: getSHNews,
   },
-  // {
-  //   name: NewsSource.AL,
-  //   logicFunction: getALNews,
-  // },
 ];
--- src/controllers/news/delete/list.ts
+++ src/controllers/news/delete/list.ts
@@ -1,15 +1,10 @@
 import { NewsSource } from "@enums";
 
 import { deleteSHNews } from "./sh";
-import { deleteALNews } from "./al";
 
 export const deleteList = [
   {
     name: NewsSource.SH,
     logicFunction: deleteSHNews,
   },
-  // {
-  //   name: NewsSource.AL,
-  //   logicFunction: deleteALNews,
-  // },
 ];
